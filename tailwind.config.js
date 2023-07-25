cefdd98f0e4d0539b451d65feadfe85f0b3aec9e
anujsnegi <139792544+anujsnegi@users.noreply.github.com>
2023-07-25 21:01:56 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      laptop: "765px",
+      
+    },
+  },
+  plugins: [],
+};
